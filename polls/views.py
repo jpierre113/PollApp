1e68f3b31f6017773305a9065cf0dfac0982fca0
Janna Pierre <gojanna@aol.com>
2020-05-03 16:27:12 -0400

    Imported Question from models

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import HttpResponse
 from django.template import loader
+from .models import Question
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
