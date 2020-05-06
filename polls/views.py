231406de30a1b1b071e00a263499e6692a1ce39e
Janna Pierre <gojanna@aol.com>
2020-05-06 17:32:17 -0400

    Imported generic

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
+
 from .models import Question
 
 # Create your views here.
